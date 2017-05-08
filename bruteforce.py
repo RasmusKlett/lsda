1de83d358e2c2d6949492c4412456c7ba1278a84
lsda <fredrik93@gmail.com>
2017-05-08 22:29:30 +0200

    bruteforce

--- bruteforce.py
+++ bruteforce.py
@@ -1,31 +1,34 @@
 import numpy
 import time
 
-docword = numpy.loadtxt("data/docword.kos.txt", skiprows=3, dtype=int)
+docword = numpy.loadtxt("docword.kos.txt", skiprows=3, dtype=int)
 
 
 def makeMatrix():
 
     start = time.time()
-    M = max(docword[:, 0])
-    N = max(docword[:, 1])
+    M = int(max(docword[:, 0]))
+    N = int(max(docword[:, 1]))
     JacMat = numpy.zeros((M, N))
 
-    for ind in range(M):
+    for ind in range(len(docword)):
         i = docword[ind, 0]
         j = docword[ind, 1]
         JacMat[i-1, j-1] = 1
 
     Corr = numpy.dot(JacMat, numpy.transpose(JacMat))
-    sim = numpy.zeros((len(Corr), len(Corr)))
+    sim = numpy.zeros((M, M))
 
     for i in range(M):
-        for j in range(i+1, N):
+        for j in range(i+1, M):
             sim[i, j] = float(Corr[i, j]) / float(Corr[i, i]+Corr[j, j]-Corr[i, j])
 
     end = time.time()
     t = end-start
     print(t)
-    return float(sum(sum(sim)))/float(M*(M-1)/2)
+    numpy.savetxt("bruteforce_sim.txt",sim)
+    avg= float(sum(sum(sim)))/float(M*(M-1)/2)
+    print(avg)
+
 
 makeMatrix()
