b415d7cf598c8201e0a90b0f685abacdcc7928a4
Rasmus Klett Mortensen <rasmus.klett@gmail.com>
2017-06-03 15:45:00 +0200

    Code for 2.1

---
+++ a5/mapper.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import sys
+
+# Where do these lines come from?
+# This is done by Hadoop Streaming ...
+for line in sys.stdin:
+
+    # Remove whitespace and split up line
+    # into words (whitespace as delimiter)
+    line = line.strip()
+
+    ignore_chars = [".", ",", "!", ":", "?"]
+    for ignore in ignore_chars:
+        line = line.replace(ignore, "")
+
+    words = line.split()
+
+    for word in words:
+
+        # The output is written to 'stdout' and will
+        # be used as input by the reducers. Hadoop
+        # Streaming will split up the work and will
+        # feed these new lines to the reducers!
+        print('%s\t%s' % (word, 1))
